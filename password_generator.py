35ab8565a2b41654a8d69ccf3c4b88430ca0c18a
x0Yukthi <yukthi100399@gmail.com>
2020-07-25 19:13:10 +0530

    Reduced Code

--- password_generator.py
+++ password_generator.py
@@ -2,10 +2,6 @@ import random as ran
 
 print("Random Password Generator!!!")
 n = int(input("Enter the length of password to be generated\n"))
-num = "1234567890"
-alpa = "abcdefghigklmnopqrstuvwxyz"
-calpa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-sp = "*^!%@$&_"
-p_s = num + alpa + calpa + sp
-pws = "".join(ran.choices(p_s, weights=None, k=n))
+password = "1234567890abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ*^!%@$&_"
+pws = "".join(ran.choices(password, weights=None, k=n))
 print("Password generated:", pws)
