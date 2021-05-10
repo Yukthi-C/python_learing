73dff856433b0db86f6f6ab940fb5bc9df6ab645
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2021-05-10 19:51:39 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,9 @@
+ad1 = input(f"Adjective1: ")
+ad2 = input(f"Adjective2: ")
+part1 = input(f"body part: ")
+dish = input(f"Dish: ")
+madlib=f"One day, a {ad1} fox invited a stork for dinner. \
+Stork was very {ad2} with the invitation – she reached the fox’s home on time and knocked at the door with her {part1}.\
+The fox took her to the dinner table and served some {dish} in shallow bowls for both of them.\
+As the bowl was too shallow for the stork, she couldn’t have soup at all. But, the fox licked up his soup quickly."
+print(f"{madlib}")
