de32335da5061520ec2ba2e5004e9d68a79fc37f
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2021-05-11 09:36:46 +0530

    Add files via upload

---
+++ Rock_Paper_.py
@@ -0,0 +1,16 @@
+import random
+
+
+def play():
+    player = input(f"Let's begin the game!!\n there are three options\nr for ROCK \np for PAPER\ns for SCISSOR\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if player == computer:
+        return 'TIE'
+    # p>r ,s>p,r>s
+    if (player == 'p' and computer == 'r') or (player == 's' and computer == 'p') or (
+            player == 'r' and computer == 's'):
+        return f"YOU WON!!!"
+
+
+print(play())
